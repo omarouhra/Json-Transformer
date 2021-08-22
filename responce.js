d2a29d3b12f947eb39d1db102f295f69f3769b0a
Omar Ouhra <omarmaroc26@gmail.com>
2021-08-22 08:35:31 +0100

    init

---
+++ responce.js
@@ -0,0 +1,32 @@
+[
+  {
+    id: 1,
+    title: "Ipad",
+    price: "900",
+    category: {
+      highLevel: "Electronics",
+      lowLevel: "Tablet",
+    },
+    salePrice: 500,
+  },
+  {
+    id: 2,
+    title: "PS5",
+    price: "600",
+    category: {
+      highLevel: "Electronics",
+      lowLevel: "Tablet",
+    },
+    salePrice: 400,
+  },
+  {
+    id: 3,
+    title: "XBOX",
+    price: "300",
+    category: {
+      highLevel: "Electronics",
+      lowLevel: "Tablet",
+    },
+    salePrice: 100,
+  },
+];
